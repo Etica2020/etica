02c1839973aa3abeb1286a26d2bc6d22621d5169
unknown <dboldure@ucm.es>
2021-01-10 21:20:06 +0100

    add: instagram, facebook, whatsapp

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,12 +1,12 @@
 var number=6;
 var random_number=0;
 link = new Array(number);
-link[0] = "http://www.amazon.es";
-link[1] = "http://www.tutores.org/";
-link[2] = "http://www.google.com";
-link[3] = "http://www.yahoo.es";
-link[4] = "http://www.facebook.com";
-link[5] = "http://www.instagram.com";
+link[0] = "tests/amazon/amazon.html";
+link[1] = "tests/facebook/facebook.html";
+link[2] = "tests/instagram/instagram.html";
+link[3] = "tests/whatsapp/whatsapp.html";
+link[4] = "tests/google/google.html";
+link[5] = "tests/twitter/twitter.html";
 function loadRandomPage(){
     random_number=Math.floor(Math.random()*(number));
     window.location=link[random_number];
